3d4585c88f1960fd8b259b07ea00241134591f29
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2014-11-04 22:01:31 +0530

    addded my_interpolate

--- DIP_Project/f_dog.m
+++ DIP_Project/f_dog.m
@@ -11,6 +11,29 @@ function [edges, He, Hg] = f_dog(I_gray, Tx, Ty, rho, sig_m, sig_c, tau, iter)
             val=lim;
         end 
     end
+    function val = my_ceil(v,lim)
+        val = ceil(v);
+        if val<1
+            val=1;
+        elseif val>lim
+            val=lim;
+        end 
+    end
+    function val = my_interp(A, x, y)
+        x1 = my_floor(x,size(A,1));
+        x2 = my_ceil(x,size(A,1));
+        y1 = my_floor(y,size(A,2));
+        y2 = my_ceil(y,size(A,2));
+        
+        
+        val1 = A(x1,y1);
+        val2 = A(x2,y2);
+        val = (val1*((x-x1)^2+(y-y1)^2) + val2*((x-x2)^2+(y-2)^2)) /((x-x1)^2+(y-y1)^2+(x-x2)^2+(y-2)^2);
+        if isnan(val)
+            val = 0;
+        end
+    end
+    
     for it=1:iter
         [M,N] = size(I_gray);
         
@@ -34,20 +57,20 @@ function [edges, He, Hg] = f_dog(I_gray, Tx, Ty, rho, sig_m, sig_c, tau, iter)
                         Hg(i,j) = Hg(i,j) + dog(0, sig_c, sig_s, rho) * I_gray(X(i,j), Y(i,j));
                     else
 
-                        in = my_floor(X(i,j),M);
-                        jn = my_floor(Y(i,j),N);
+                        in = my_interp(Tx, X(i,j),Y(i,j));
+                        jn = my_interp(Ty, X(i,j),Y(i,j));
 
-                        X(i,j) = X(i,j) - Ty(in,jn);
-                        Y(i,j) = Y(i,j) + Tx(in,jn);
+                        X(i,j) = X(i,j) - jn;
+                        Y(i,j) = Y(i,j) + in;
 
-                        in = my_floor(Xn(i,j),M);
-                        jn = my_floor(Yn(i,j),N);     
+                        in = my_interp(Tx, Xn(i,j),Yn(i,j));
+                        jn = my_interp(Ty, Xn(i,j),Yn(i,j));    
 
-                        Xn(i,j) = Xn(i,j) + Ty(in,jn);
-                        Yn(i,j) = Yn(i,j) - Tx(in,jn);
+                        Xn(i,j) = Xn(i,j) + jn;
+                        Yn(i,j) = Yn(i,j) - in;
 
     %                     [i,j,X(i,j),Y(i,j),Xn(i,j),Yn(i,j)]
-                        Hg(i,j) = Hg(i,j) + dog(a, sig_c, sig_s, rho) * (I_gray(my_floor(X(i,j),M), my_floor(Y(i,j),N)) + I_gray(my_floor(Xn(i,j),M), my_floor(Yn(i,j),N)));
+                        Hg(i,j) = Hg(i,j) + dog(a, sig_c, sig_s, rho) * (my_interp(I_gray, X(i,j),Y(i,j)) + my_interp(I_gray, Xn(i,j),Yn(i,j)));
                     end
                 end
             end
@@ -64,17 +87,19 @@ function [edges, He, Hg] = f_dog(I_gray, Tx, Ty, rho, sig_m, sig_c, tau, iter)
                         Yn(i,j) = j;
                         He(i,j) = He(i,j) + normpdf(0, 0, sig_m) * Hg(X(i,j), Y(i,j));
                     else
-                        in = my_floor(X(i,j),M);
-                        jn = my_floor(Y(i,j),N);
-                        X(i,j) = X(i,j) + Tx(in,jn);
-                        Y(i,j) = Y(i,j) + Ty(in,jn);
+                        in = my_interp(Tx, X(i,j),Y(i,j));
+                        jn = my_interp(Ty, X(i,j),Y(i,j));
 
-                        in = my_floor(Xn(i,j),M);
-                        jn = my_floor(Yn(i,j),N);                
-                        Xn(i,j) = Xn(i,j) - Tx(in,jn);
-                        Yn(i,j) = Yn(i,j) - Ty(in,jn);
+                        X(i,j) = X(i,j) + in;
+                        Y(i,j) = Y(i,j) + jn;
 
-                        He(i,j) = He(i,j) + normpdf(a,0,sig_m) * (Hg(my_floor(X(i,j),M), my_floor(Y(i,j),N)) + Hg(my_floor(Xn(i,j),M), my_floor(Yn(i,j),N)));
+                        in = my_interp(Tx, Xn(i,j),Yn(i,j));
+                        jn = my_interp(Ty, Xn(i,j),Yn(i,j));    
+
+                        Xn(i,j) = Xn(i,j) - in;
+                        Yn(i,j) = Yn(i,j) - jn;
+
+                        He(i,j) = He(i,j) + normpdf(a,0,sig_m) * (my_interp(Hg, X(i,j),Y(i,j)) + my_interp(Hg, Xn(i,j),Yn(i,j)));
                     end
                 end
             end
